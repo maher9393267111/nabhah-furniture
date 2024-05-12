2d134c13d96aaba9ea1e460c8b4427a47ae9c404
maher9393267111 <89878167+maher9393267111@users.noreply.github.com>
2024-05-12 16:45:32 +0300

    add

--- pages/products/single.jsx
+++ pages/products/single.jsx
@@ -6,61 +6,54 @@ import Image from "next/image";
 import { NextSeo } from "next-seo";
 import { useRouter } from "next/router";
 import Link from "next/link";
-import {useState ,useEffect} from 'react'
+import { useState, useEffect } from "react";
 import { useAuth } from "@/functions/context";
 import Loader from "@/components/common/Loader";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import { useTranslation } from "react-i18next";
 
 const NFT = ({}) => {
+  const [product, setProduct] = useState({});
+  console.log("🎭🎭🎭>", product.title);
+  //  const [loacding, setLoading] = useState(false);
+  const { pageLoading, setPageLoading } = useAuth();
+  const { t } = useTranslation();
 
+  const router = useRouter();
+  const locale = router.locale;
+  const id = router.query.id;
 
+  //const { t } = useTranslation();
 
-const [product, setProduct] = useState({});
-console.log("🎭🎭🎭>", product.title);
-//  const [loacding, setLoading] = useState(false);
-const {pageLoading, setPageLoading} = useAuth()
-const {t}= useTranslation()
+  useEffect(() => {
+    const getProduct = async () => {
+      // setLoading(true);
+      setPageLoading(true);
+      //setProduct({});
+      const data = await getDocument("products", id);
+      console.log(data, "fetch categories ====>>> 🎭🎭🎭>", data);
+      setProduct(data);
+      setPageLoading(false);
+      // setLoading(false);
+    };
 
-const router = useRouter();
-const locale = router.locale;
-const id = router.query.id;
+    if (id) getProduct();
+  }, [id]);
 
-//const { t } = useTranslation();
+  const dir = router.locale === "ar" && "rtl";
 
-useEffect(() => {
-  const getProduct = async () => {
-   // setLoading(true);
-   setPageLoading(true)
-    //setProduct({});
-    const data = await getDocument("products", id);
-    console.log(data, "fetch categories ====>>> 🎭🎭🎭>", data);
-    setProduct(data);
-    setPageLoading(false)
-   // setLoading(false);
-  };
-
-  if (id) getProduct();
-}, [id]);
-
-
-
-
-
-const dir =router.locale === 'ar' && 'rtl'
-
-if (!product ){
-
-    return <div><h1>No Product Founded</h1></div>
-}
-
-
-
-
+  if (!product) {
     return (
-        <Layout>
-          <div>
-      {/* <NextSeo
+      <div>
+        <h1>No Product Founded</h1>
+      </div>
+    );
+  }
+
+  return (
+    <Layout>
+      <div>
+        {/* <NextSeo
         title={product?.title + " | Sandalye" + " | Ayka Chair"}
         description={"İnegöl Sandalye " + product?.title + " Model"}
         canonical={router.asPath}
@@ -83,45 +76,44 @@ if (!product ){
           cardType: "summary_large_image",
         }}
       /> */}
-      <div>
-
-        {product?.images &&
-        <section>
-          <div className="relative mx-auto max-w-7xl px-4 py-8 sm:px-6 lg:px-8">
-            <div className="grid grid-cols-1 items-start gap-8 md:grid-cols-2 ">
-              <div className="relative order-2 grid grid-cols-1 gap-4 md:order-none">
-
-
-                <div className="aspect-w-1 aspect-h-1 ">
-                  <Image
-                    className="bg-grad rounded-xl !h-[500px] "
-                    src={product?.images[0]}
-                    alt={product?.title}
-                    layout="responsive"
-                    width={"700"}
-                    height={"700"}
-                    objectFit="cover"
-                  />
-                </div>
-
-                <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 lg:mt-4">
-                  {product?.images?.map((image ,index) => (
-                    <div
-                      key={index}
-                      className="aspect-w-1 aspect-h-1 hover:z-50"
-                    >
-                      <img
-                        className="rounded-xl h-[200px] w-full md:!w-[288px] md:!h-[200px] border object-cover transition hover:scale-150 "
-                        src={image}
+        <div>
+          {product?.images && (
+            <section>
+              <div className="relative mx-auto max-w-7xl px-4 py-8 sm:px-6 lg:px-8">
+                <div className="grid grid-cols-1 items-start gap-8 md:grid-cols-2 ">
+                  <div className="relative order-2 grid grid-cols-1 gap-4 md:order-none">
+                    <div className="aspect-w-1 aspect-h-1 ">
+                      <Image
+                        className="bg-grad rounded-xl !h-[500px] "
+                        src={product?.images[0]}
+                        alt={product?.title}
+                        layout="responsive"
+                        width={"700"}
+                        height={"700"}
+                        objectFit="cover"
                       />
                     </div>
-                  ))}
-                </div>
-              </div>
-              <div className="top-8 block md:sticky">
 
-              <div dir={dir} className="mt-8 gap-3 flex lg:mt-0 lg:flex-shrink-0">
-{/* 
+                    <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 lg:mt-4">
+                      {product?.images?.map((image, index) => (
+                        <div
+                          key={index}
+                          className="aspect-w-1 aspect-h-1 hover:z-50"
+                        >
+                          <img
+                            className="rounded-xl h-[200px] w-full md:!w-[288px] md:!h-[200px] border object-cover transition hover:scale-150 "
+                            src={image}
+                          />
+                        </div>
+                      ))}
+                    </div>
+                  </div>
+                  <div className="top-8 block md:sticky">
+                    <div
+                      dir={dir}
+                      className="mt-8 gap-3 flex lg:mt-0 lg:flex-shrink-0"
+                    >
+                      {/* 
 <div className="inline-flex rounded-md shadow">
     
       <p className="inline-flex arabic items-center justify-center rounded-md border border-transparent bg-primary px-5 py-3 text-base font-medium text-black ">
@@ -130,73 +122,70 @@ if (!product ){
     
   </div> */}
 
-  
-  <div className="inline-flex rounded-md shadow">
- 
-      <a
-      href="https://wtspee.com/905522982310" target="_blank"
-      className="inline-flex arabic items-center justify-center rounded-md border border-transparent bg-primary px-5 py-3 text-base font-medium text-black ">
-      {t('whatsapptitle')}
-      </a>
-    
-  </div>
-  <div className="ml-3 inline-flex rounded-md shadow">
-    <Link legacyBehavior href="/products">
-      <a className="inline-flex arabic items-center justify-center rounded-md border border-transparent bg-white px-5 py-3 text-base font-medium text-rose-600 hover:bg-rose-50">
-        {t('shopgo')}
-      </a>
-    </Link>
-  </div>
-</div>
+                      <div className="inline-flex rounded-md shadow">
+                        <a
+                          href="https://wtspee.com/905522982310"
+                          target="_blank"
+                          className="inline-flex arabic items-center justify-center rounded-md border border-transparent bg-primary px-5 py-3 text-base font-medium text-black "
+                        >
+                          {t("whatsapptitle")}
+                        </a>
+                      </div>
+                      <div className="ml-3 inline-flex rounded-md shadow">
+                        <Link legacyBehavior href="/products">
+                          <a className="inline-flex arabic items-center justify-center rounded-md border border-transparent bg-white px-5 py-3 text-base font-medium text-rose-600 hover:bg-rose-50">
+                            {t("shopgo")}
+                          </a>
+                        </Link>
+                      </div>
+                    </div>
 
+                    <h1 dir={dir} className="text-5xl arabic font-bold  mt-12">
+                      {router?.locale === "ar"
+                        ? product?.titlear
+                        : router.locale === "en"
+                        ? product?.title
+                        : product?.titletr}
+                    </h1>
+                    <section className="group relative mt-4">
+                      {product?.desc && (
+                        <article
+                          dir={dir}
+                          className="prose arabic text-xl max-w-none pb-6"
+                        >
+                          {router?.locale === "ar"
+                            ? product?.descar
+                            : router.locale === "en"
+                            ? product?.desc
+                            : product?.desctr}
+                          {/* <RichText content={chair.content.raw} /> */}
 
-
-
-                <h1 dir={dir} className="text-5xl arabic font-bold  mt-12">{ router?.locale === 'ar' ?  product?.titlear : router.locale === 'en' ? product?.title : product?.titletr }</h1>
-                <section className="group relative mt-4">
-                  {product?.desc && (
-                    <article dir={dir} className="prose arabic text-xl max-w-none pb-6">
-                        { router?.locale === 'ar' ?  product?.descar : router.locale === 'en' ? product?.desc : product?.desctr }
-                      {/* <RichText content={chair.content.raw} /> */}
-
-
-                      <div  dir={dir} className="inline-fle rounded-md shado">
-    
-    <p className="inline-flex arabic items-center justify-center rounded-md border border-transparent bg-primary px-5 py-3 text-xl mt-12 font-medium text-black ">
-   { router?.locale === 'ar' ?  "السعر" : router.locale === 'en' ? "Price" : "Fiyat"}:   {product?.price}$
-    </p>
-  
-</div>
-
-
-                    </article>
-
-
-
-
-
-                  )}
-
-                  
-
-
-                  
-                </section>
+                          <div
+                            dir={dir}
+                            className="inline-fle rounded-md shado"
+                          >
+                            <p className="inline-flex arabic items-center justify-center rounded-md border border-transparent bg-primary px-5 py-3 text-xl mt-12 font-medium text-black ">
+                              {router?.locale === "ar"
+                                ? "السعر"
+                                : router.locale === "en"
+                                ? "Price"
+                                : "Fiyat"}
+                              : {product?.price} TL
+                            </p>
+                          </div>
+                        </article>
+                      )}
+                    </section>
+                  </div>
+                </div>
               </div>
-            </div>
-          </div>
-        </section>
-}
+            </section>
+          )}
+        </div>
       </div>
-    </div>
-        
-        
     </Layout>
-    )
-}
-
-
-
+  );
+};
 
 export const getStaticProps = async ({ locale }) => {
   //const allProducts = await api.getAllProducts();
@@ -207,9 +196,6 @@ export const getStaticProps = async ({ locale }) => {
     },
   };
 };
-
-
-
 
 // const getNFTbyId = async (id) => {
 //     const nftDocument = doc(db, `products`, id);
@@ -229,6 +215,5 @@ export const getStaticProps = async ({ locale }) => {
 //         nft : nftData,
 //     }
 // }
-
 
 export default NFT;
